f0e15d5c11b6951fc5e710e5219436823b1e6212
Sam Umirdinov <sam.umirdinov@gmail.com>
2019-04-23 16:52:58 -0700

    gets imdbID and passes to axios

--- server/app.js
+++ server/app.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const morgan = require('morgan');
+const axios = require('axios');
+
+let imdbID;
 
 
 const app = express();
 
-// When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
+app.use(morgan('dev'));
+
+app.get('/', function(req, res){
+    imdbID = req.query.i;
+   if (imdbID) {
+       console.log(imdbID);
+    axios
+    .get('http://www.omdbapi.com/?i=' + imdbID + '&apikey=8730e0e')
+    .then( (response) => {
+        res.send(response.data);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+   }
+
+});  
 
 module.exports = app;
