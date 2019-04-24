2d4d251d70e721a53a3f1ea70b9d825366c11e37
Sam Umirdinov <sam.umirdinov@gmail.com>
2019-04-23 18:39:08 -0700

    creates temporary cache but doest work yet

--- server/app.js
+++ server/app.js
@@ -3,7 +3,8 @@ const morgan = require('morgan');
 const axios = require('axios');
 
 let imdbID;
-
+// let data;
+const m = new Map();
 
 const app = express();
 
@@ -11,12 +12,15 @@ app.use(morgan('dev'));
 
 app.get('/', function(req, res){
     imdbID = req.query.i;
-   if (imdbID) {
-       console.log(imdbID);
+   if (imdbID && m.has('data')) {
+       return m;
+    } else {
     axios
     .get('http://www.omdbapi.com/?i=' + imdbID + '&apikey=8730e0e')
     .then( (response) => {
         res.send(response.data);
+        
+        m.set('data', response.data);
     })
     .catch((error) => {
         console.log(error);
@@ -24,5 +28,7 @@ app.get('/', function(req, res){
    }
 
 });  
+
+
 
 module.exports = app;
